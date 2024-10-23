f1758c03a062dedb8cc810c668386a42f6d76ac0
Tobiasz Bilinski <Tobiasz.Bilinski@partner.bmw.de>
2024-10-23 22:01:24 +0200

    Create div for categories thumbnails

--- src/app/components/Categories.js
+++ src/app/components/Categories.js
@@ -2,6 +2,8 @@ import styles from "../components/Categories.module.css";
 
 export default function Categories() {
   return <>
-    
+    <div className={styles["categories-container"]}>
+        <div className={styles["categories"]}></div>
+    </div>
   </>;
 }
