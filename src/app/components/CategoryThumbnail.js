27b185aa9bd9de6b30a3e964fef632330c3cbf05
Tobiasz Bilinski <Tobiasz.Bilinski@partner.bmw.de>
2024-11-01 14:12:17 +0100

    Add shadow under categories, fix height and width for SeeProductBox

--- src/app/components/CategoryThumbnail.js
+++ src/app/components/CategoryThumbnail.js
@@ -5,11 +5,16 @@ export default function CategoryThumbnail(props) {
   return (
     <>
       <div className="grid grid-rows-categories bg-[#f1f1f1] overflow-visible h-[284px] justify-items-center">
-          <div className="bg-white h-16 w-full"></div>
-          <div className={props.image === "/categories_earphones.png" ? "relative h-28 -top-[4em]" : "relative h-28 -top-[5em]"}> {/* zmien to zeby bylo przekazywane w propsach */}
-            <Image src={props.image} alt={props.alt} width={props.width} height={props.height} />
-          </div>
-          <p className="text-fs-18 tracking-129 font-bold">{props.categoryName}</p>
+        <Image className="absolute" src="/OvalShadow.png" alt="Oval Shadow" width={350} height={263} />
+        <div className="bg-white h-16 w-full"></div>
+        <div className={`relative h-28 ${props.topValue}`}>
+          <Image src={props.image} alt={props.alt} width={props.width} height={props.height} />
+        </div>
+        <p className="text-fs-18 tracking-129 font-bold">{props.categoryName}</p>
+        <div className="grid grid-cols-2 gap-4 items-center">
+          <p className="text-fs-13 tracking-100 opacity-50 font-bold ">SHOP</p>
+          <Image src="/Arrow.png" alt="Orange arrow" width={8} height={18} />
+        </div>
       </div>
     </>
   );
