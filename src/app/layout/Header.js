5cbeef46b5bf25a806fc7cc0f49aba634b287ec6
Tobbil <kysthalden@gmail.com>
2024-11-11 20:51:19 +0100

    Add Playwright and first test

--- src/app/layout/Header.js
+++ src/app/layout/Header.js
@@ -7,34 +7,46 @@ export default function Header() {
     <>
       <header className="m-0 w-screen bg-[#141414] justify-items-center">
         <div className="relative pt-[2rem] pr-[10rem] pb-[2rem] pl-[10rem]">
-          <div className="grid grid-cols-3 text-white h-[2rem] content-center">
-            <div className="justify-self-start self-center">
-              <a href="#">
-                <Image src="/audiophile_text.png" alt="Audiophile logo" width={143} height={25} />
-              </a>
-            </div>
-            <div className="w-full flex justify-center">
-              <div className="grid grid-flow-col auto-cols-min gap-8 text-fs-13 font-semibold tracking-200 text-[#f2f2f2] content-center">
-                <a className={menuHover} href="#">
-                  HOME
+          <nav>
+            <div className="grid grid-cols-3 text-white h-[2rem] content-center">
+              <div className="justify-self-start self-center">
+                <a href="#">
+                  <Image
+                    src="/audiophile_text.png"
+                    alt="Audiophile logo"
+                    width={143}
+                    height={25}
+                  />
                 </a>
-                <a className={menuHover} href="#">
-                  HEADPHONES
-                </a>
-                <a className={menuHover} href="#">
-                  SPEAKERS
-                </a>
-                <a className={menuHover} href="#">
-                  EARPHONES
+              </div>
+              <div className="w-full flex justify-center">
+                <div className="grid grid-flow-col auto-cols-min gap-8 text-fs-13 font-semibold tracking-200 text-[#f2f2f2] content-center">
+                  <a className={menuHover} href="#">
+                    HOME
+                  </a>
+                  <a className={menuHover} href="#">
+                    HEADPHONES
+                  </a>
+                  <a className={menuHover} href="#">
+                    SPEAKERS
+                  </a>
+                  <a className={menuHover} href="#">
+                    EARPHONES
+                  </a>
+                </div>
+              </div>
+              <div className="justify-self-end self-center">
+                <a href="#">
+                  <Image
+                    src="/basket_icon.png"
+                    alt="Basket icon"
+                    width={23.33}
+                    height={20}
+                  />
                 </a>
               </div>
             </div>
-            <div className="justify-self-end self-center">
-              <a href="#">
-                <Image src="/basket_icon.png" alt="Basket icon" width={23.33} height={20} />
-              </a>
-            </div>
-          </div>
+          </nav>
           <div className="mt-9 opacity-20 mb-[0.1em]">
             <hr />
           </div>
