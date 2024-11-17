750a43bf0b5bbf5c2dad6208085918f8a0bc4de2
Tobbil <kysthalden@gmail.com>
2024-11-17 22:37:39 +0100

    Add photo to 2 column feautured item (temporarily hardcoding)

--- src/app/components/FeaturedItemGrey2Cols.js
+++ src/app/components/FeaturedItemGrey2Cols.js
@@ -1,12 +1,15 @@
 import "tailwindcss/tailwind.css";
 import SeeProductBoxNoDesc from "./SeeProductBoxGreyNoDesc";
+import Image from "next/image";
 
-export default function FeaturedItemGrey() {
+export default function FeaturedItemGrey2Cols() {
   return (
     <>
       <section className="self-center">
         <div className="grid justify-center grid-cols-2 h-[20rem] w-[69rem] mt-8 mb-8 gap-6">
-        <div className=""></div>
+        <div className="">
+          <Image src="/FeaturedYX1.png" alt="YX1 earphones" width={530} height={320} />
+        </div>
           <div className="bg-grey flex items-center justify-center pb-4">
             <SeeProductBoxNoDesc
               dimensions="h-300 w-350"
