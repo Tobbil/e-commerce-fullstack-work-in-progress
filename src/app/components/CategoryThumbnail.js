85998e3a2055313ec9a1a2103816697ec087911f
Tobbil <kysthalden@gmail.com>
2025-01-05 21:55:25 +0100

    Fix padding in SeeProductBox, add highlight on entire CategoryThumbnail element

--- src/app/components/CategoryThumbnail.js
+++ src/app/components/CategoryThumbnail.js
@@ -5,30 +5,31 @@ import Link from "next/link";
 export default function CategoryThumbnail(props) {
   return (
     <>
-      <div className="overflow-visible h-[20rem] w-[14rem] lg:w-[22rem] relative">
-        <Image
-          className="absolute bottom-16 lg:top-10"
-          src="/OvalShadow.png"
-          alt="Oval Shadow"
-          width={350}
-          height={263}
-        />
-        <div className="bg-white h-24 w-full"></div>
-        <div className="bg-[#f1f1f1] w-full h-[12.75rem] rounded-lg flex flex-col items-center">
-          <div className={`relative h-28 ${props.topValue}`}>
-            <Image
-              src={props.image}
-              alt={props.alt}
-              width={props.width}
-              height={props.height}
-            />
-          </div>
-          <p className="text-fs-18 tracking-129 font-bold">
-            {props.categoryName}
-          </p>
-          <Link href={props.categoryUrl}>
+      <Link href={props.categoryUrl}>
+        <div className="overflow-visible h-[19rem] w-[14rem] lg:w-[22rem] relative hover:text-orange hover:opacity-100">
+          <Image
+            className="absolute bottom-16 lg:top-10"
+            src="/OvalShadow.png"
+            alt="Oval Shadow"
+            width={350}
+            height={263}
+          />
+          <div className="bg-white h-24 w-full"></div>
+          <div className="bg-[#f1f1f1] w-full h-[12.75rem] rounded-lg flex flex-col items-center">
+            <div className={`relative h-28 ${props.topValue}`}>
+              <Image
+                src={props.image}
+                alt={props.alt}
+                width={props.width}
+                height={props.height}
+              />
+            </div>
+            <p className="text-fs-18 tracking-129 font-bold">
+              {props.categoryName}
+            </p>
+
             <div className="relative flex gap-2 pt-4">
-              <p className="text-fs-13 tracking-100 opacity-50 font-bold hover:text-orange hover:opacity-100">
+              <p className="text-fs-13 tracking-100 opacity-50 font-bold">
                 SHOP
               </p>
               <Image
@@ -38,9 +39,9 @@ export default function CategoryThumbnail(props) {
                 height={12}
               />
             </div>
-          </Link>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
